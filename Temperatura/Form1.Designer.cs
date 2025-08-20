6e9d4f7a7f8c0ba6cfe180f1ddf42fd36c84c68a
rflszk <faelsuzuki000@gmail.com>
2025-08-19 22:01:23 -0300

    Feito interface, tratamentos de erro

--- Temperatura/Form1.Designer.cs
+++ Temperatura/Form1.Designer.cs
@@ -28,13 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.lbltempo = new System.Windows.Forms.Label();
+            this.txbtempo = new System.Windows.Forms.TextBox();
+            this.btnclima = new System.Windows.Forms.Button();
+            this.pibimagem = new System.Windows.Forms.PictureBox();
+            this.lbldata = new System.Windows.Forms.Label();
+            this.lblresultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pibimagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(142, 38);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(515, 39);
+            this.lbltitulo.TabIndex = 0;
+            this.lbltitulo.Text = "Verificador de Temperatura2000";
+            // 
+            // lbltempo
+            // 
+            this.lbltempo.AutoSize = true;
+            this.lbltempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltempo.Location = new System.Drawing.Point(12, 126);
+            this.lbltempo.Name = "lbltempo";
+            this.lbltempo.Size = new System.Drawing.Size(212, 25);
+            this.lbltempo.TabIndex = 1;
+            this.lbltempo.Text = "Digite a temperatura:";
+            // 
+            // txbtempo
+            // 
+            this.txbtempo.Location = new System.Drawing.Point(230, 131);
+            this.txbtempo.Name = "txbtempo";
+            this.txbtempo.Size = new System.Drawing.Size(315, 20);
+            this.txbtempo.TabIndex = 2;
+            // 
+            // btnclima
+            // 
+            this.btnclima.Location = new System.Drawing.Point(219, 157);
+            this.btnclima.Name = "btnclima";
+            this.btnclima.Size = new System.Drawing.Size(341, 46);
+            this.btnclima.TabIndex = 3;
+            this.btnclima.Text = "Verificar Clima";
+            this.btnclima.UseVisualStyleBackColor = true;
+            this.btnclima.Click += new System.EventHandler(this.btnclima_Click);
+            // 
+            // pibimagem
+            // 
+            this.pibimagem.Location = new System.Drawing.Point(230, 209);
+            this.pibimagem.Name = "pibimagem";
+            this.pibimagem.Size = new System.Drawing.Size(315, 241);
+            this.pibimagem.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pibimagem.TabIndex = 4;
+            this.pibimagem.TabStop = false;
+            this.pibimagem.Click += new System.EventHandler(this.pibimagem_Click);
+            // 
+            // lbldata
+            // 
+            this.lbldata.AutoSize = true;
+            this.lbldata.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldata.Location = new System.Drawing.Point(288, 91);
+            this.lbldata.Name = "lbldata";
+            this.lbldata.Size = new System.Drawing.Size(70, 25);
+            this.lbldata.TabIndex = 5;
+            this.lbldata.Text = "label1";
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(144, 493);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(0, 25);
+            this.lblresultado.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 551);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.lbldata);
+            this.Controls.Add(this.pibimagem);
+            this.Controls.Add(this.btnclima);
+            this.Controls.Add(this.txbtempo);
+            this.Controls.Add(this.lbltempo);
+            this.Controls.Add(this.lbltitulo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pibimagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.Label lbltempo;
+        private System.Windows.Forms.TextBox txbtempo;
+        private System.Windows.Forms.Button btnclima;
+        private System.Windows.Forms.PictureBox pibimagem;
+        private System.Windows.Forms.Label lbldata;
+        private System.Windows.Forms.Label lblresultado;
     }
 }
 
--- Temperatura/Form1.cs
+++ Temperatura/Form1.cs
@@ -15,6 +15,47 @@ namespace Temperatura
         public Form1()
         {
             InitializeComponent();
+            lbldata.Text = DateTime.Now.ToString();
+        }
+
+        private void pibimagem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnclima_Click(object sender, EventArgs e)
+        {
+            
+
+            if (lbltempo.Text == "")
+            {
+                MessageBox.Show("Por favor Digite a temperatura!", "erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                int tempo = int.Parse(txbtempo.Text);
+                if (tempo < 15)
+                {
+                    pibimagem.Image = Properties.Resources.snow;
+                    lblresultado.Text = $"A temperatura de hoje é {tempo}ºC! O clima está frio!";
+                }
+                else if(tempo < 30)
+                {
+                    pibimagem.Image = Properties.Resources.clouds;
+                    lblresultado.Text = $"A temperatura de hoje é {tempo}ºC! O clima está agradável!";
+                }
+                else if (tempo >= 30)
+                {
+                    pibimagem.Image = Properties.Resources.sun;
+                    lblresultado.Text = $"A temperatura de hoje é {tempo}ºC! O clima está quente!";
+                }
+                else
+                {
+                    MessageBox.Show("O medidor de temperatura não pode calcular corretamente!");
+                } 
+                    
+            }
+        }
         }
     }
-}
+
